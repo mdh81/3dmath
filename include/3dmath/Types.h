93b951191c82885c22bceb9375f2f0846ab37428
mdh81 <mdh81>
2023-04-15 18:33:27 -0700

    Beef up bounding box implementation so it can be used by mesh viewer

--- include/3dmath/Types.h
+++ include/3dmath/Types.h
@@ -8,6 +8,7 @@ namespace math3d {
         T min;
         T max;
         T length() const { return max - min; }
+        T center() const { 0.5f*(max + min); }
     };
 
     template<typename T>
@@ -15,6 +16,38 @@ namespace math3d {
         Extent<T> x;
         Extent<T> y;
         Extent<T> z;
+
+        Bounds3D(std::initializer_list<std::initializer_list<T>> const& initializerList){
+            x.min = data(data(initializerList)[0])[0];
+            x.max = data(data(initializerList)[0])[1];
+
+            y.min = data(data(initializerList)[1])[0];
+            y.max = data(data(initializerList)[1])[1];
+
+            z.min = data(data(initializerList)[2])[0];
+            z.max = data(data(initializerList)[2])[1];
+        }
+
+        Bounds3D() {
+            x.min = std::numeric_limits<float>::max();
+            y.min = std::numeric_limits<float>::max();
+            z.min = std::numeric_limits<float>::max();
+            x.max = -x.min;
+            y.max = -y.min;
+            z.max = -z.min;
+        }
+
+        // Builds a symmetric bounding box where each side is
+        // of the specified length
+        explicit Bounds3D(T const sideLength) {
+            x.min = -0.5f * sideLength;
+            x.max = +0.5f * sideLength;
+            y.min = -0.5f * sideLength;
+            y.max = +0.5f * sideLength;
+            z.min = -0.5f * sideLength;
+            z.max = +0.5f * sideLength;
+        }
+
     };
 
     constexpr float tolerance = 1e-6;
