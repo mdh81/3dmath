820c3136e590c1551f8747f2e19769b06b5afb8f
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-24 11:00:59 -0700

    Derivation of ray-ray intersection

--- include/3dmath/primitives/Ray.h
+++ include/3dmath/primitives/Ray.h
@@ -3,11 +3,19 @@
 #include "Primitive.h"
 
 namespace math3d {
+    // Ray has an origin and a direction. The direction is normalized and the ray is
+    // assumed to extend to infinity from the origin
     class Ray : public Primitive {
     public:
         Ray() = default;
 
         ~Ray() = default;
+
+
+        // Please refer to 
+        Point intersectWithRay(const Ray &ray) override {
+            return {};
+        }
 
         [[nodiscard]]
         Vector3D<float> const &getDirection() const {
