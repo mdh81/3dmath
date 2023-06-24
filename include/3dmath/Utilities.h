9b169b628727ee36272989db58af19f44f37a59c
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-24 12:10:12 -0700

    Derivation for distance of a point to a ray

--- include/3dmath/Utilities.h
+++ include/3dmath/Utilities.h
@@ -1,7 +1,24 @@
 #pragma once
 #include "Constants.h"
+#include <cmath>
+
 namespace math3d {
     inline float asDegrees(float radians) {
         return radians * radiansToDegrees;
     };
+
+    template<typename T>
+    bool isZero(T const val) {
+        static_assert(std::is_floating_point<T>(),
+                      "Only floating point types can be compared to zero using this function");
+        return std::fabs(val) < tolerance;
+    }
+
+    template<typename T1, typename T2>
+    bool areEqual(T1 const val1, T2 const val2) {
+        static_assert(std::is_same<T1, T2>(), "Cannot compare dissimilar types");
+        static_assert(std::is_floating_point<T1>(),
+                      "Only floating point types can be compared using this function");
+        return isZero(std::fabs(val2-val1));
+    }
 }
--- include/3dmath/primitives/Ray.h
+++ include/3dmath/primitives/Ray.h
@@ -1,20 +1,49 @@
 #pragma once
 
 #include "Primitive.h"
+#include "../Utilities.h"
 
 namespace math3d {
     // Ray has an origin and a direction. The direction is normalized and the ray is
     // assumed to extend to infinity from the origin
     class Ray : public Primitive {
     public:
-        Ray() = default;
+        Ray(Point const& origin, Vector3D<float> const& direction) {
+            vertices.push_back(origin);
+            this->direction = direction;
+            this->direction.normalize();
+        }
 
         ~Ray() = default;
 
+        float distanceToPoint(Point const& point) const {
+            auto originToPoint = point - getOrigin();
+            float angle = originToPoint.dot(direction);
+            return sin(angle) * originToPoint.length();
+        }
 
         // Please refer to
-        Point intersectWithRay(const Ray &ray) override {
-            return {};
+        // https://github.com/mdh81/3dmath/raw/master/derivations/RayRayIntersection.jpg
+        IntersectionResult intersectWithRay(Ray const& ray) override {
+            IntersectionResult result;
+
+            // Check if rays are parallel
+            auto d1xd2 = this->direction * ray.direction;
+            if (isZero(d1xd2.length())) {
+                result.status = IntersectionStatus::NoIntersection;
+            }
+
+            // Parametric distance along ray to the intersection point
+            auto d1xd2Length = d1xd2.length();
+            float t = ((ray.getOrigin() - this->getOrigin()).dot(d1xd2)) / (d1xd2Length * d1xd2Length);
+
+            // Intersection point
+            result.intersectionPoint = getOrigin() + (t * direction);
+
+            // Check if the intersection point is on the line by computing the perpendicular distance between this ray
+            // and the point
+
+
         }
 
         [[nodiscard]]
