5931c0a8cd8e21e216e9d7e41475eb94f53e36c7
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-24 20:53:16 -0700

    Put constants in a nested namespace called Constants to clarify scope and improve readability

--- include/3dmath/primitives/Sphere.h
+++ include/3dmath/primitives/Sphere.h
@@ -64,8 +64,8 @@ namespace math3d {
             // In spherical coordinate system, phi = zero is a singularity, where theta has no influence on
             // the resulting coordinate. To prevent this, we start with phi at a sensible value given our resolution
             // constraint.
-            float phiIncrement = oneEightyDegrees / static_cast<float>(resolution);
-            float thetaIncrement = threeSixtyDegrees / static_cast<float>(resolution);
+            float phiIncrement = Constants::oneEightyDegrees / static_cast<float>(resolution);
+            float thetaIncrement = Constants::threeSixtyDegrees / static_cast<float>(resolution);
             float phi = phiIncrement, theta = 0.f;
 
             // Rotation by theta in spherical by convention rotates about the +z-axis. When theta is zero, the projection
@@ -85,7 +85,7 @@ namespace math3d {
 
             // Pole 2: At distance of radius from the center along -z
             vertices.emplace_back(origin +
-                                  SphericalCoordinates{radius, 0.f, oneEightyDegrees}.getCartesianCoordinates());
+                                  SphericalCoordinates{radius, 0.f,  Constants::oneEightyDegrees}.getCartesianCoordinates());
 
             // Generate faces
             // NOTE: In the two lambdas below, indices are vertex indices
