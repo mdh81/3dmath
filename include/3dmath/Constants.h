fe6d6e9e6aa5bc0b5bde8eb80eff447d4bbb957e
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-24 20:10:30 -0700

    Add convenience constants for cardinal axis

--- include/3dmath/Constants.h
+++ include/3dmath/Constants.h
@@ -7,4 +7,7 @@ namespace math3d {
     constexpr float oneEightyDegrees = std::numbers::pi;
     constexpr float radiansToDegrees = 180 / std::numbers::pi;
     constexpr float tolerance = 1e-6;
+    constexpr std::initializer_list<float> xAxis = {1.f, 0.f, 0.f};
+    constexpr std::initializer_list<float> yAxis = {0.f, 1.f, 0.f};
+    constexpr std::initializer_list<float> zAxis = {0.f, 0.f, 1.f};
 }
