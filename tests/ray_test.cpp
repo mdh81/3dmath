6f5ae8f4bf373a6badf31dc438daa049502a4f9c
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-24 17:30:56 -0700

    Conform to fully qualified name usage

--- tests/ray_test.cpp
+++ tests/ray_test.cpp
@@ -1,17 +1,17 @@
 #include "gtest/gtest.h"
 #include "3dmath/primitives/Ray.h"
-using namespace std;
-using namespace math3d;
 
-TEST(Ray, DistanceToPoint) {
+TEST(Ray, PerpendicularDistanceToPoint) {
     // Same direction
-    Ray r({0,0,0}, {1,0,0});
+    math3d::Ray r({0,0,0}, {1,0,0});
     ASSERT_FLOAT_EQ(10, r.distanceToPoint({10, 10, 0}));
 
     // Different direction
     ASSERT_FLOAT_EQ(10, r.distanceToPoint({-10, -10, 0}));
 
-    // TODO: Different plane
-    //Vector3D<float> point = {-10, -10, 10};
-    //ASSERT_FLOAT_EQ(point.length(), r.distanceToPoint(point));
+    // Special cases:
+}
+
+TEST(Ray, DistanceToPointOnDifferentPlane) {
+
 }
