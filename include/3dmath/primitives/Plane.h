1fbdb5200218659f5ef90d03a434eee7891ac540
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2023-06-30 09:59:01 -0700

    WIP: Primitive tests

--- include/3dmath/primitives/Plane.h
+++ include/3dmath/primitives/Plane.h
@@ -62,14 +62,44 @@ namespace math3d {
             tris.emplace_back(2, 3, 0);
         }
 
+        // Compute projection of the given point on this plane
+        Point getProjection(Point const& point) {
+            auto planeOriginToPoint = point - origin;
+            auto distanceToPlane = planeOriginToPoint.dot(normal);
+            return planeOriginToPoint - distanceToPlane * normal;
+        }
+
         // TODO: Sketch this
         IntersectionResult intersectWithRay(const Ray &ray) override {
+            // A ray and a plane cannot intersect when the ray is parallel to the plane
+            // and the ray origin is not on the plane. Otherwise, the ray will
+            // intersect the plane
+            IntersectionResult result;
+            result.status = IntersectionStatus::Intersects;
+
+            auto planeToRayOrigin = ray.getOrigin() - origin;
+            auto distanceToRayOrigin = planeToRayOrigin.dot(normal);
             // Check if point is on the plane
+            if (Utilities::isZero(distanceToRayOrigin)) {
+                result.intersectionPoint = ray.getOrigin();
+            } else {
+                auto rayOriginProjectedOnPlane = ray.getOrigin() - distanceToRayOrigin * normal;
+                auto projectionOfRayOnPlaneNormal = ray.getDirection().dot(normal);
+
+
+            }
             return IntersectionResult();
         }
 
     private:
         Vector normal;
         unsigned const geometrySize;
+
+    friend std::ostream& operator<<(std::ostream& os, Plane const& plane);
     };
+
+    inline std::ostream& operator<<(std::ostream& os, Plane const& plane) {
+        os << "Origin: " << plane.getOrigin() << " Normal: " << plane.getNormal();
+        return os;
+    }
 }
--- tests/plane_test.cpp
+++ tests/plane_test.cpp
@@ -2,8 +2,7 @@
 #include "3dmath/primitives/Plane.h"
 #include "TestSupport.h"
 
-TEST(Plane, Construction) {
-}
+constexpr unsigned robustnessTestSampleCount = 100;
 
 TEST(Plane, Getters) {
     math3d::Primitive::Point origin  = math3d::Utilities::RandomPoint();
@@ -31,5 +30,29 @@ TEST(Plane, GeometryGeneration) {
                 math3d::test::TestSupport::areBinarySTLFilesEqual(
                         fileName,
                         std::filesystem::path("baseline") / fileName)) << "Geometry in STL file is different";
+    }
+}
+
+TEST(Plane, PointProjection) {
+    math3d::Plane plane {math3d::constants::origin, math3d::constants::zAxis};
+    auto projectedPoint = plane.getProjection({0, 0, 5});
+    ASSERT_FLOAT_EQ(projectedPoint.x, 0.f);
+    ASSERT_FLOAT_EQ(projectedPoint.y, 0.f);
+    ASSERT_FLOAT_EQ(projectedPoint.z, 0.f);
+}
+
+TEST(Plane, PointProjectionRobustness) {
+    for (auto sample = 1u; sample <= robustnessTestSampleCount; ++sample) {
+        math3d::ConvexPrimitive::Point pointInSpace = math3d::Utilities::RandomPoint();
+        math3d::ConvexPrimitive::Point planeOrigin = math3d::Utilities::RandomPoint();
+        math3d::ConvexPrimitive::Vector planeNormal = math3d::Utilities::RandomVector();
+        auto plane = math3d::Plane{planeOrigin, planeNormal};
+        auto projectedPoint = plane.getProjection(pointInSpace);
+        auto planeVector = projectedPoint - planeOrigin;
+        if (!math3d::Utilities::isZero(planeVector.dot(planeNormal))) {
+            std::cerr << "Plane projection failed for plane " << plane << " and point " << pointInSpace << std::endl;
+            std::cerr << "Projected point " << projectedPoint << std::endl;
+            plane.writeToSTL("ProjectionFailure.stl");
+        }
     }
 }
