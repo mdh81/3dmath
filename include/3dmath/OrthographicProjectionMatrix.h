36a589b05005cf2c0857930e18a471b149203299
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2024-07-11 14:12:11 -0700

    Fixed a bug in the orthographic projection calculation by making sure the returned matrix works correctly for offset bounding volumes. Previously, the z-coordinates were incorrect if the bounding volume was offset in the z direction. The logic to invert z-coordinates for a left-handed output system wasn't inverting the z-value of the translation vector. Also, replaced explicit inversion request with non-template parameter that allows the user to specify the output system's handedness

--- include/3dmath/OrthographicProjectionMatrix.h
+++ include/3dmath/OrthographicProjectionMatrix.h
@@ -3,20 +3,19 @@
 #include "ProjectionMatrix.h"
 namespace math3d {
 
-template<typename DataType>
+template<typename DataType, CoordinateSystemHandedness OutputSystemHandedness>
 class OrthographicProjectionMatrix : public ProjectionMatrix<DataType> {
 
 public:
     // Allow users to create an empty matrix and then call update when the projection needs to be recomputed
     OrthographicProjectionMatrix() = default;
 
-    // OpenGL uses a left-handed system for normalized device coordinates, so z has to be inverted
-    OrthographicProjectionMatrix(Bounds3D<DataType> const& bounds3D, bool invertZ = true)
+    OrthographicProjectionMatrix(Bounds3D<DataType> const& bounds3D)
     : ProjectionMatrix<DataType>() {
-        update(bounds3D, invertZ);
+        update(bounds3D);
     }
 
-    void update(Bounds3D<DataType> const& bounds3D, bool invertZ = true) override {
+    void update(Bounds3D<DataType> const& bounds3D) override {
 
         auto& data = Matrix<DataType, 4u, 4u>::data;
 
@@ -28,10 +27,18 @@ public:
         // See https://mdh81.github.io/3dmath/orthographicProjection/ for a derivation for this matrix
         data[0]  = 2 / bounds3D.x.length();
         data[5]  = 2 / bounds3D.y.length();
-        data[10] = (2 / bounds3D.z.length()) * (invertZ ? -1 : 1);
+        data[10] = 2 / bounds3D.z.length();
         data[12] = -(bounds3D.x.min + bounds3D.x.max) / bounds3D.x.length();
         data[13] = -(bounds3D.y.min + bounds3D.y.max) / bounds3D.y.length();
         data[14] = -(bounds3D.z.min + bounds3D.z.max) / bounds3D.z.length();
+
+        // This library uses a right-handed coordinate system, but a graphics system like OpenGL might require projection
+        // matrices set up so that the clip coordinates are in a left-handed system. In that case, z-coordinates have to
+        // be inverted
+        if (OutputSystemHandedness == CoordinateSystemHandedness::LeftHanded) {
+            data[10] *= -1;
+            data[14] *= -1;
+        }
     }
 
 };
