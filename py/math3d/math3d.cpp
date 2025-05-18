9eeb9abfc2c813f6d6f06a91eb1c9aec03ead055
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2025-05-17 20:28:45 -0700

    Add python binding support for n-d vectors

--- py/math3d/math3d.cpp
+++ py/math3d/math3d.cpp
@@ -1,8 +1,12 @@
 #include "pybind11/pybind11.h"
-
-void bind_Vector3d(pybind11::module_ const&);
+#include "vector.hpp"
 
 PYBIND11_MODULE(math3d, module) {
     auto const vectorModule = module.def_submodule("vector");
-    bind_Vector3d(vectorModule);
+    bind_Vector<double, 3>(vectorModule, "vector3");
+    bind_Vector<double, 4>(vectorModule, "vector4");
+    bind_Vector<double, 2>(vectorModule, "vector2");
+    bind_Vector<float, 3>(vectorModule, "vector3f");
+    bind_Vector<float, 4>(vectorModule, "vector4f");
+    bind_Vector<float, 2>(vectorModule, "vector2f");
 }
---
+++ py/math3d/vector.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include "pybind11/pybind11.h"
+#include "pybind11/operators.h"
+#include "pybind11/stl.h"
+
+#include "Vector.h"
+
+namespace py = pybind11;
+
+template<typename T, uint32_t Size>
+void bind_Vector(py::module_ const& module, char const* className) {
+    using vector_n = math3d::Vector<T, Size>;
+    auto vec_class =
+        py::class_<vector_n>(module, className)
+        // Construction
+        .def(py::init())
+        .def(py::init([](py::list const& list) {
+            auto const input = list.cast<std::vector<T>>();
+            return vector_n(input);
+        }))
+        // Member access
+        .def_property("x",
+            [](vector_n const& self) {
+                double const value = self.x;
+                return value;
+            },
+            [](vector_n& self, double const value) {
+                self.x = value;
+            }
+        )
+        .def_property("y",
+        [](vector_n const& self) {
+                T const value = self.y;
+                return value;
+            },
+            [](vector_n& self, double const value) {
+                self.y = value;
+            }
+        )
+        .def_property("z",
+        [](vector_n const& self) {
+                T const value = self.z;
+                return value;
+            },
+            [](vector_n& self, double const value) {
+                self.z = value;
+            }
+        )
+        // Formatted output
+        .def("__str__", &vector_n::asString)
+        .def("__repr__", &vector_n::asString)
+        // Operations
+        .def(py::self + py::self)
+        .def(py::self - py::self)
+        .def(py::self * T{})
+        .def(py::self / T{})
+        .def("dot", &vector_n::dot)
+        .def("normalize", [](vector_n& v) { v.normalize(); return v; })
+        .def("length", [](vector_n const& v) { return v.length(); })
+        .def("length_sqr", [](vector_n const& v) { return v.lengthSquared(); })
+        .def("projection", &vector_n::getVectorProjection);
+    if constexpr (Size == 3) {
+        vec_class
+        .def(py::self * py::self);
+    }
+}
