9a54c11367dbf43bde9111fdd672dd19393c20e2
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2026-01-17 21:36:33 -0800

    Cleaned up incompatible property accessors for composite vector types. Stick to PascalCase for c++ names for consistency with rest of the codebase

--- py/math3d/vector.hpp
+++ py/math3d/vector.hpp
@@ -10,7 +10,7 @@ namespace py = pybind11;
 template<typename T, uint32_t Size>
 void bind_Vector(py::module_ const& module, char const* className) {
     using vector = math3d::Vector<T, Size>;
-    auto vec_class =
+    auto pyVecClass =
         py::class_<vector>(module, className)
         // Construction
         .def(py::init())
@@ -22,34 +22,6 @@ void bind_Vector(py::module_ const& module, char const* className) {
             std::vector<T> input {vec3.x, vec3.y, vec3.z, 1.F};
             return vector{input};
         }))
-        // Member access
-        .def_property("x",
-            [](vector const& self) {
-                double const value = self.x;
-                return value;
-            },
-            [](vector& self, double const value) {
-                self.x = value;
-            }
-        )
-        .def_property("y",
-        [](vector const& self) {
-                T const value = self.y;
-                return value;
-            },
-            [](vector& self, double const value) {
-                self.y = value;
-            }
-        )
-        .def_property("z",
-        [](vector const& self) {
-                T const value = self.z;
-                return value;
-            },
-            [](vector& self, double const value) {
-                self.z = value;
-            }
-        )
         // Formatted output
         .def("__str__", [](vector const& v) {
             return util::convertSpaceToNewLine(v.asString());
@@ -66,18 +38,64 @@ void bind_Vector(py::module_ const& module, char const* className) {
         .def("normalize", [](vector& v) { v.normalize(); return v; })
         .def("length", [](vector const& v) { return v.length(); })
         .def("length_sqr", [](vector const& v) { return v.lengthSquared(); })
-        .def("projection", &vector::getVectorProjection);
+        .def("projection", [](vector const& self, vector const& u) {
+            auto vectorProjection = self.getVectorProjection(u);
+            return std::pair{vectorProjection.parallel, vectorProjection.perpendicular};
+        });
     if constexpr (Size == 3) {
-        vec_class
+        pyVecClass
         .def(py::init([](T x, T y, T z) {
             return vector({x, y, z});
         }))
         .def(py::self * py::self);
     }
     if constexpr (Size == 4) {
-        vec_class
+        pyVecClass
         .def(py::init([](T x, T y, T z, T w) {
             return vector({x, y, z, w});
         }));
     }
+
+    // Convenience member access (x, y, z, w)
+    // .x, .y, .z, .w are Vector<T, N>::Proxy and its conversion operator has to be invoked to get the raw value
+    // hence the static cast
+    if constexpr (Size >= 2 && Size <= 4) {
+        pyVecClass.def_property("x",
+            [](vector const& self) {
+                    return static_cast<T>(self.x);
+                },
+            [](vector& self, T value) {
+                    self.x = value;
+                }
+        )
+        .def_property("y",
+            [](vector const& self) {
+                    return static_cast<T>(self.y);
+                },
+            [](vector& self, T value) {
+                    self.y = value;
+                }
+        );
+    }
+    if constexpr (Size == 3 || Size == 4 ) {
+        pyVecClass.def_property("z",
+            [](vector const& self) {
+                    return static_cast<T>(self.z);
+                },
+            [](vector& self, T value) {
+                    self.z = value;
+                }
+        );
+    }
+    if constexpr (Size == 4) {
+        pyVecClass.def_property("w",
+            [](vector const& self) {
+                    return static_cast<T>(self.w);
+                },
+            [](vector& self, T value) {
+                    self.w = value;
+                }
+        );
+    }
+
 }
